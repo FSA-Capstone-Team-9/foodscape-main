8daaef56504dd6d51f2528167e8c5d8d47bffc91
John Lee <lee.john.tk@gmail.com>
2021-07-07 17:18:42 -0500

    cleaned up map component

--- client/components/map.js
+++ client/components/map.js
@@ -1,8 +1,4 @@
 import React, { useRef, useEffect, useState } from 'react';
-// TODO
-// import 'mapbox-gl/dist/mapbox-gl.css';
-// import '../../public/style.css';
-
 import mapboxgl from '!mapbox-gl';
 import axios from 'axios';
 
