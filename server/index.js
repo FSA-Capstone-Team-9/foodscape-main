ade172c8f0e3cfbd530da499502c58214ccdd914
Joshua Johnson <jjohn8@unh.newhaven.edu>
2021-07-09 17:05:32 -0400

    implemented restaurant request method

--- server/index.js
+++ server/index.js
@@ -1,28 +1,33 @@
-const express = require("express")
-const { ApolloServer } = require("apollo-server-express")
-const path = require("path")
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
 // Construct a schema, using GraphQL schema language
 // Provide type definitions and resolver functions for your schema fields
-const resolvers = require("./resolvers")
-const typeDefs = require("./typeDefs")
+const resolvers = require("./resolvers");
+const typeDefs = require("./typeDefs");
 
 // initiate server
-const server = new ApolloServer({ typeDefs, resolvers })
-const app = express()
+const server = new ApolloServer({ typeDefs, resolvers });
+const app = express();
 
 // For Heroku - Only require dotenv when NODE_ENV is set to development
-if (process.env.NODE_ENV == 'development')
-require('dotenv').config({ silent: true });
+if (process.env.NODE_ENV == "development") {
+  require("dotenv").config({ silent: true });
+}
 
 //utilize express as middleware for the server
-server.applyMiddleware({ app })
+server.applyMiddleware({ app });
 
-app.use(express.static(path.join(__dirname, "..", "public")))
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.get("/", (req, res) =>
-    res.sendFile(path.join(__dirname, "..", "public/index.html"))
-)
+  res.sendFile(path.join(__dirname, "..", "public/index.html"))
+);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () =>
-    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`)
-)
+  console.log(
+    `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
+  )
+);
+
+module.exports = app;
---
+++ server/routes.js
@@ -0,0 +1,44 @@
+// require("dotenv").config();
+// const YELP_API_KEY = process.env.YELP_API_KEY;
+// const axios = require("axios");
+// const searching = async (location) => {
+//   try {
+//     console.log(YELP_API_KEY);
+//     const apiEndpoint = `https://api.yelp.com/v3/businesses/search/?term=food&location=${location}`;
+//     const response = await axios.get(apiEndpoint, {
+//       headers: {
+//         Authorization: `Bearer ${YELP_API_KEY}`,
+//       },
+//     });
+//     console.log(response.businesses);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// searching("NYC");
+
+// Yelp Fusion Module
+const yelp = require("yelp-fusion");
+require("dotenv").config();
+
+const YELP_API_KEY = process.env.YELP_API_KEY;
+const client = yelp.client(YELP_API_KEY);
+
+// sample request
+const searchRequest = {
+  term: "food",
+  location: "new york, ny",
+};
+
+const searching = async () => {
+  try {
+    const results = await client.search(searchRequest);
+    // sample output
+    results.jsonBody.businesses.forEach((result) => console.log(result.name));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+searching();
