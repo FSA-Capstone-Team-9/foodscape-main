3fb4e2a5361332f0078de4a4f40808646045fc14
John Lee <lee.john.tk@gmail.com>
2021-07-09 11:51:57 -0500

    prod: refactored port for Heroku

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ app.get("/", (req, res) =>
     res.sendFile(path.join(__dirname, "..", "public/index.html"))
 )
 
-app.listen({ port: 4000 }, () =>
-    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () =>
+    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`)
 )
