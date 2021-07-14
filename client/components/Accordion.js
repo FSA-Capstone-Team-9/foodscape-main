e2b8379c0a42d4e505cd408b7c4220f73ecdd8b2
a-hsu <a.hsu873@gmail.com>
2021-07-14 12:46:10 -0400

    feat: wip - search autocompletion and fixing block element issues with MUI

--- client/components/Accordion.js
+++ client/components/Accordion.js
@@ -24,7 +24,7 @@ const useStyles = makeStyles(theme => ({
 
 export default function SimpleAccordion(props) {
     const classes = useStyles()
-    const [value, setValue] = React.useState("female")
+    const [value, setValue] = React.useState("1")
 
     const handleChange = event => {
         setValue(event.target.value)
--- client/components/Search-Bar.js
+++ client/components/Search-Bar.js
@@ -1,117 +1,170 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
+import React, { useState } from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import Paper from "@material-ui/core/Paper"
+import InputBase from "@material-ui/core/InputBase"
+import Divider from "@material-ui/core/Divider"
+import IconButton from "@material-ui/core/IconButton"
+import SearchIcon from "@material-ui/icons/Search"
 
 // for alert
-import Snackbar from '@material-ui/core/Snackbar';
-import MuiAlert from '@material-ui/lab/Alert';
+import Snackbar from "@material-ui/core/Snackbar"
+import MuiAlert from "@material-ui/lab/Alert"
 
-import axios from 'axios';
+// autocomplete
+import TextField from "@material-ui/core/TextField"
+import Autocomplete from "@material-ui/lab/Autocomplete"
 
-export const useStyles = makeStyles((theme) => ({
-  root: {
-    padding: '2px 4px',
-    display: 'flex',
-    alignItems: 'center',
-    width: 600,
-  },
-  input: {
-    marginLeft: theme.spacing(1),
-    flex: 1,
-  },
-  iconButton: {
-    padding: 10,
-  },
-  divider: {
-    height: 28,
-    margin: 4,
-  },
-}));
+import axios from "axios"
+
+export const useStyles = makeStyles(theme => ({
+    root: {
+        padding: "2px 4px",
+        display: "flex",
+        alignItems: "center",
+        width: "600px",
+        margin: "10px",
+    },
+    input: {
+        marginLeft: theme.spacing(1),
+        flex: 1,
+    },
+    iconButton: {
+        padding: 10,
+    },
+    divider: {
+        height: 28,
+        margin: 4,
+    },
+}))
 
 export default function SearchBar(props) {
-  function Alert(props) {
-    return <MuiAlert elevation={6} variant="filled" {...props} />;
-  }
-
-  const notificationTimer = 4000; // ms
-
-  const classes = useStyles();
-
-  const [searchTerms, setSearchTerms] = useState('');
-  const [location, setLocation] = useState('');
-  const [open, setOpen] = React.useState(false);
-
-  const badSearch = () => {
-    setOpen(true);
-  };
-
-  const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
+    function Alert(props) {
+        return <MuiAlert elevation={6} variant="filled" {...props} />
     }
-    setOpen(false);
-  };
 
-  async function handleSubmit(event) {
-    event.preventDefault();
-    try {
-      const result = await axios.get(
-        `https://api.tiles.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
-          location
-        )}.json?access_token=${process.env.MAPBOX_TOKEN}`
-      );
-      console.log('Mapbox Geolocate API results -->', result)
-      const coordinates = result.data.features[0].center // [longitude,latitude]
-      props.handleSearchSubmit(coordinates, searchTerms)
-    } catch (error) {
-      badSearch();
-      console.log(error);
+    const notificationTimer = 4000 // ms
+
+    const classes = useStyles()
+
+    const [inputValue, setInputValue] = useState("")
+    const [value, setValue] = useState("")
+    const [searchTerms, setSearchTerms] = useState("")
+    const [location, setLocation] = useState("")
+    const [open, setOpen] = useState(false)
+    const [results, setResults] = useState([])
+    const [choice, setChoice] = useState(0)
+
+    const badSearch = () => {
+        setOpen(true)
     }
-  }
 
-  return (
-    <React.Fragment>
-      <Paper component="form" className={classes.root}>
-        <h3>Cuisine</h3>
-        <InputBase
-          className={classes.input}
-          placeholder="Food Type"
-          inputProps={{ 'aria-label': 'Food Type' }}
-          value={searchTerms}
-          onChange={(event) => setSearchTerms(event.target.value)}
-        />
-        <Divider className={classes.divider} orientation="vertical" />
-        <h3>At</h3>
-        <InputBase
-          className={classes.input}
-          placeholder="Location"
-          inputProps={{ 'aria-label': 'Location' }}
-          value={location}
-          onChange={(event) => setLocation(event.target.value)}
-        />
-        <IconButton
-          onClick={handleSubmit}
-          type="submit"
-          className={classes.iconButton}
-          aria-label="search"
-        >
-          <SearchIcon />
-        </IconButton>
-      </Paper>
-      <Snackbar
-        open={open}
-        autoHideDuration={notificationTimer}
-        onClose={handleClose}
-      >
-        <Alert onClose={handleClose} severity="warning">
-          We couldn't find the the location you were looking for. Please try
-          again!
-        </Alert>
-      </Snackbar>
-    </React.Fragment>
-  );
+    const handleClose = (event, reason) => {
+        if (reason === "clickaway") {
+            return
+        }
+        setOpen(false)
+    }
+
+    async function handleSubmit(event) {
+        event.preventDefault()
+        try {
+            console.log(location)
+            const result = await axios.get(
+                `https://api.tiles.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+                    location
+                )}.json?access_token=${process.env.MAPBOX_TOKEN}&country=us`
+            )
+            console.log("Mapbox Geolocate API results -->", result)
+            console.log("choice", choice)
+            console.log("result:", result.data.features)
+            const coordinates = result.data.features[choice].center // [longitude,latitude]
+            props.handleSearchSubmit(coordinates, searchTerms)
+        } catch (error) {
+            badSearch()
+            console.log(error)
+        }
+    }
+    async function onSearchChange(event) {
+        setLocation(event.target.value)
+        try {
+            const result = await axios.get(
+                `https://api.tiles.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+                    location
+                )}.json?access_token=${process.env.MAPBOX_TOKEN}&country=us`
+            )
+            setResults(result.data.features)
+        } catch (error) {}
+    }
+
+    return (
+        <React.Fragment>
+            <Paper component="form" className={classes.root}>
+                {/* <h3>Cuisine</h3> */}
+                <TextField
+                    className={classes.input}
+                    placeholder="Cuisine/Food Type"
+                    inputProps={{ "aria-label": "Food Type" }}
+                    // variant="outlined"
+                    // margin="normal"
+                    value={searchTerms}
+                    onChange={event => setSearchTerms(event.target.value)}
+                />
+                {/* <Divider className={classes.divider} orientation="vertical" /> */}
+                {/* <h3>At</h3> */}
+                <Autocomplete
+                    freeSolo
+                    className={classes.input}
+                    id="location"
+                    disableClearable
+                    options={results.map((results, index) => {
+                        return results.place_name
+                    })}
+                    value={value}
+                    onChange={(event, newValue) => {
+                        setChoice(event.target.dataset.optionIndex)
+                        setValue(newValue)
+                        setLocation(newValue)
+                    }}
+                    inputValue={inputValue}
+                    onInputChange={(event, newInputValue) => {
+                        onSearchChange(event)
+                        setInputValue(newInputValue)
+                    }}
+                    renderInput={params => (
+                        <TextField
+                            {...params}
+                            placeholder="Location"
+                            // label="Search input"
+                            value={location}
+                            // margin="normal"
+                            // variant="outlined"
+                            InputProps={{
+                                ...params.InputProps,
+                                type: "search",
+                            }}
+                        />
+                    )}
+                />
+
+                <IconButton
+                    onClick={handleSubmit}
+                    type="submit"
+                    className={classes.iconButton}
+                    aria-label="search"
+                >
+                    <SearchIcon />
+                </IconButton>
+            </Paper>
+            <Snackbar
+                open={open}
+                autoHideDuration={notificationTimer}
+                onClose={handleClose}
+            >
+                <Alert onClose={handleClose} severity="warning">
+                    We couldn't find the the location you were looking for.
+                    Please try again!
+                </Alert>
+            </Snackbar>
+        </React.Fragment>
+    )
 }
