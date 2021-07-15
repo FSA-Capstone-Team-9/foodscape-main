56d43a119386bc2ecab8550235e14be0847f19b8
Dylan <dylanlovespie@gmail.com>
2021-07-15 12:40:26 -0400

    changing component sizes

--- client/components/Search-Bar.js
+++ client/components/Search-Bar.js
@@ -21,7 +21,7 @@ export const useStyles = makeStyles(theme => ({
         padding: "2px 4px",
         display: "flex",
         alignItems: "center",
-        width: 600,
+        width: '100%',
         margin: 5,
     },
     input: {
